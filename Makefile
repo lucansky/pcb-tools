e0e8dcb96ca759db887193cd206f57ffed27eb78
Adam Lucansky <adamlucansky@gmail.com>
2018-02-01 02:56:13 +0100

    Initial paper

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ all: demo
 demo:
 	stack build && stack exec -- drawgerber --width=400 -o test.pdf
 
+grammar:
+	grep -- '-- >' src/Data/Gerber/Parser.hs | sed 's/^.\{5\}//g' | tr -s '::=' '&' | column -t -s'&' | awk 'sub(/[>]/, "> ::=")' > docs/gerber.ebnf
+
 bnfc:
 	bnfc --haskell grammar/GerberAST.cf -p Data.Gerber -d -o src/
 	sed -i '' 's/Main/Data\.Gerber\.GerberAST\.Test/g' src/Data/Gerber/GerberAST/Test.hs
--- src/Data/Gerber/Parser.hs
+++ src/Data/Gerber/Parser.hs
@@ -20,29 +20,67 @@ parseGerber input = parseOnly (parseGerber' <* endOfInput) input
 
 nl = optionalNewLines
 
+-- > <S> ::= <gerberCommands>
+-- > <gerberCommands> ::= {"%" <extended> "%" | <standard> "*" | <eof> }
+-- > <char>   ::= any ASCII char
+-- > <eof>    ::= "M02*" {<anyChar>}
 parseGerber' :: Parser [Command]
 parseGerber' = many1 $ (eof <|> e <|> s) <* optionalNewLines
     where
-      e   = (char '%' *> parseExtendedCommand <* char '%') <* nl
-      s   = parseStandardCommand <* char '*' <* nl
-      eof = EndOfFile <$ (string "M02" <* char '*' <* takeWhile (const True))
+      extended = (char '%' *> parseExtendedCommand <* char '%') <* nl
+      standard = parseStandardCommand <* char '*' <* nl
+      eof      = EndOfFile <$ (string "M02" <* char '*' <* takeWhile (const True))
 
-eat x = takeWhile1 $ inClass x
-
+-- > <anyCharExceptAsterisk> ::= [ASCII] - ["*"]
+-- > <allowedChars> ::= A-Za-z0-9,.#@$\n
 allowedChars :: String
 allowedChars = "A-Za-z0-9,.#@$\n"
 
+-- Low-level parsers
+eat x = takeWhile1 $ inClass x
+
+-- > <optionalNewLines> ::= {"\n" | "\r"}
+optionalNewLines = many (char '\n' <|> char '\r')
+
+-- > <takeTillAsteriskMany> ::= (<anyCharExceptAsterisk>)* "*"
+-- > <takeTillAsteriskMany1> ::= <anyCharExceptAsterisk> <takeTillAsteriskMany>
+restOfCommand = takeWhile (/='*')
+
+-- Consumes single block extended command.
+-- Basically drops '*' to make all subsequent parsers simpler.
+-- > <singleBlockWrap> ::= <singleBlockExtendedCommand> "*"
+singleBlock :: Parser a -> Parser a
+singleBlock p = p <* char '*'
+
+-- Last '*' in extended command is to be handled manually.
+-- > <multiBlockWrap> ::= <multiBlockExtendedCommand>
+multiBlock p = p
+
+--
+-- > <standard> ::= <comment>
+-- > <standard> ::= <toolChange>
+-- > <standard> ::= <operation>
+-- > <standard> ::= <quadrantMode>
+-- > <standard> ::= <interpolationMode>
+-- > <standard> ::= <regionBoundary>
+-- > <standard> ::= <unknownStandard>
 parseStandardCommand :: Parser Command
 parseStandardCommand =
   choice $ [parseComment,
             parseToolChange,
-            parseDCode,
+            parseOperation,
             SetQuadrantMode <$> parseQuadrantMode,
             SetInterpolationMode <$> parseInterpolationMode,
             SetRegionBoundary <$> parseRegionBoundary,
             --EndOfFile <$ (string "M02"),
             parseUnknownStandard]
 
+-- > <extended> ::= <singleBlockCommand> "*" | <multiBlockCommand>
+-- > <singleBlockCommand> ::= <formatSpecification>
+-- > <singleBlockCommand> ::= <setUnits>
+-- > <singleBlockCommand> ::= <addAperture>
+-- > <singleBlockCommand> ::= <unknownExtended>
+-- > <multiBlockCommands> ::= <apertureMacro>
 parseExtendedCommand :: Parser Command
 parseExtendedCommand =
   parseApertureMacro <|> choice singleBlockParsers
@@ -53,55 +91,52 @@ parseExtendedCommand =
                           parseUnknownExtended]
 
 ---
+
+-- > <quadrantMode> ::= "G74" | "G75"
 parseQuadrantMode :: Parser QuadrantMode
 parseQuadrantMode = SingleQuadrant <$ "G74"
                 <|> MultiQuadrant <$ "G75"
 
+-- > <interpolationMode> ::= "G01" | "G02" | "G03"
 parseInterpolationMode :: Parser InterpolationMode
 parseInterpolationMode =
   Linear           <$ "G01" <|>
   Clockwise        <$ "G02" <|>
   CounterClockwise <$ "G03"
 
+-- > <regionBoundary> ::= "G36" | "G37"
 parseRegionBoundary = StartRegion <$ "G36"
                   <|> EndRegion <$ "G37"
 
--- Low-level parsers
-optionalNewLines = many (char '\n' <|> char '\r')
-restOfCommand = takeWhile (/='*')
 
+-- > <comment> ::= "G04 " <commentChars> "*"
+-- > <commentChars> ::= [ASCII] - ["*"]
 parseComment :: Parser Command
 parseComment = Comment <$> ("G04" *> (char ' ') *> restOfCommand)
 
+-- > <toolChange> ::= "D" integer {integer}
 parseToolChange :: Parser Command
 parseToolChange = ToolChange <$> (char 'D' *> num)
 
+-- > <action> ::= "D01" | "D02" | "D03"
 parseAction :: Parser Action
 parseAction = Draw <$ "D01"
           <|> Move <$ "D02"
           <|> Flash <$ "D03"
 
-coords = Coord <$> maybeOption (char 'X' *> num) <*> maybeOption (char 'Y' *> num)
+-- > <coord> ::= ["X" integer] ["Y" integer] ["I" integer] ["J" integer]
+coords = Coord <$> maybeOption (char 'X' *> num) <*> maybeOption (char 'Y' *> num) -- FIXME: I,J
 
-parseDCode :: Parser Command
-parseDCode = Operation <$> coords <*> parseAction
+-- > <operation> ::= <coord> <action>
+parseOperation :: Parser Command
+parseOperation = Operation <$> coords <*> parseAction
 
-parseEOF :: Parser Command
-parseEOF = EndOfFile <$ "M02"
-
+-- > <unknownStandard> ::= (<anyCharExceptAsterisk>)* "*"
 parseUnknownStandard :: Parser Command
 parseUnknownStandard = UnknownStandard <$> eat allowedChars
 
-
--- Consumes single block extended command.
--- Basically drops '*' to make all subsequent parsers simpler.
-singleBlock :: Parser a -> Parser a
-singleBlock p = p <* char '*'
-
--- Last '*' in extended command is to be handled manually.
-multiBlock p = p
-
 ---
+-- > <unknownExtended> ::= (<anyCharExceptAsterisk>)* "*"
 parseUnknownExtended :: Parser Command
 parseUnknownExtended = UnknownExtended <$> takeWhile1 (/='*')
 
@@ -110,6 +145,7 @@ parseUnknownExtended = UnknownExtended <$> takeWhile1 (/='*')
 maybeOption :: Parser a -> Parser (Maybe a)
 maybeOption p = option Nothing (Just <$> p)
 
+-- > <formtSpecification> ::= "FSLA" "X" digit digit "Y" digit digit
 parseFormatSpecification :: Parser Command
 parseFormatSpecification = do
   "FSLA"
@@ -122,14 +158,20 @@ parseFormatSpecification = do
   return $ FormatStatement $ FormatSpecification (z xM) (z xM)
   where z = toInteger.digitToInt
 
+-- > <setUnits> ::= "MO" ("MM" | "IN")
 parseSetUnits :: Parser Command
 parseSetUnits = SetUnits <$> (string "MO" *> ((MM <$ "MM") <|> (IN <$ "IN")))
 
+-- > <addAperture> ::= "ADD" integer ([A-Z0-9]+) "," ({scientific "X"} | scientific)
 parseAddAperture :: Parser Command
 parseAddAperture = AddAperture <$> ("ADD" *> decimal)
-                               <*> takeWhile (inClass "A-Z0-9") <* char ','
+                               <*> takeWhile1 (inClass "A-Z0-9") <* char ','
                                <*> sepBy scientific (char 'X')
 
+-- > <apertureMacro> ::= "AM" <allowedChars>* "*" <apertures>
+-- > <apertures> ::= <singleAperture> {<singleAperture>}
+-- > <singleAperture> ::= <allowedChars>* "*" <optionalNewLines>
+-- FIXME: parses, but does not give a meaning to the data
 parseApertureMacro :: Parser Command
 parseApertureMacro = do
   string "AM"
